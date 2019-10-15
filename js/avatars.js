4fb886fbac46ce959586d9ff73d834504828d147
midevelop <mi@elbrusboot.camp>
2019-10-15 19:41:45 +0300

    Work version

---
+++ js/avatars.js
@@ -0,0 +1,27 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    title: "Еноты",
+    current: 0,
+    show: false,
+    people: [],
+    info: ""
+  },
+  mounted() {
+    axios
+      .get("/docs/raccoons.json")
+      .then(response => (this.people = response.data));
+  },
+  computed: {
+    showPeople() {
+      return this.people.slice(this.current, this.current + 3);
+    }
+  },
+  methods: {
+    nextAvatar() {
+      this.show = false;
+      if (this.current > this.people.length - 4) this.current = 0;
+      else this.current++;
+    }
+  }
+});
