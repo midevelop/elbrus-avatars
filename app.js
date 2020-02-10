c68ac3efb0cae4b7b3a8d8854949e6d3b317e34f
midevelop <mi@elbrusboot.camp>
2020-02-10 11:45:28 +0300

    feat(project): docs add docs pathd

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const express = require("express")
-const morgan = require("morgan")
+const express = require("express");
+const morgan = require("morgan");
 
-const app = express()
-app.use(morgan("dev"))
+const app = express();
+app.use(morgan("dev"));
 
-const PORT = process.env.PORT || 3000
-    app.listen(PORT, ()=> {console.log(`Сервер запущен на порту ${PORT}`)})
+app.use(express.static(__dirname + "/public"));
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
